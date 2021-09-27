0ef9ef679b8311f1ccc7aec22fac027294805047
Egon Willighagen <egon.willighagen@gmail.com>
2021-09-27 16:28:22 +0200

    First iteration of the RDF generation script

---
+++ convert2rdf.groovy
@@ -0,0 +1,74 @@
+@Grab(group='io.github.egonw.bacting', module='managers-cdk', version='0.0.24')
+@Grab(group='io.github.egonw.bacting', module='managers-inchi', version='0.0.24')
+
+import groovy.io.FileType
+import groovy.json.JsonSlurper
+
+workspaceRoot = "."
+def cdk = new net.bioclipse.managers.CDKManager(workspaceRoot);
+def inchi = new net.bioclipse.managers.InChIManager(workspaceRoot);
+
+File folder = new File('mibig_json_2.0/')
+
+println "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
+println "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
+println "PREFIX cito: <http://purl.org/spar/cito/>"
+
+println "PREFIX sso: <http://semanticscience.org/resource/>"
+println "PREFIX smiles: <http://semanticscience.org/resource/CHEMINF_000018>"
+println "PREFIX inchikey: <http://semanticscience.org/resource/CHEMINF_000059>"
+
+println "PREFIX bigcat: <http://www.bigcat.unimaas.nl/mibig-rdf/onto/>"
+println "PREFIX cluster: <http://www.bigcat.unimaas.nl/mibig-rdf/cluster/>"
+println "PREFIX cmpd: <http://www.bigcat.unimaas.nl/mibig-rdf/compound/>"
+
+println "PREFIX ncbitaxon: <http://www.identifiers.org/ncbitaxon/>"
+println "PREFIX inchikeyuri: <http://www.identifiers.org/inchikey/>"
+println "PREFIX pubmed: <http://www.identifiers.org/pubmed/>"
+println "";
+
+folder.eachFileRecurse FileType.FILES,  { file ->
+  if (file.name.endsWith(".json")) {
+    clusterID = file.name.replace(".json","")
+    println "# Processing file ${clusterID}"
+    def jsonSlurper = new JsonSlurper()
+    def data = jsonSlurper.parseText(file.text)
+    println "cluster:${clusterID} a bigcat:GeneCluster ;"
+
+    // compounds
+    cmpdData = "";
+    data.cluster.compounds.each { compound ->
+      // println "#  compound SMILES: ${compound.chem_struct}"
+      mol = cdk.fromSMILES(compound.chem_struct)
+      molInChI = inchi.generate(mol)
+      molURI = "inchikeyuri:${molInChI.key}"
+      println "  bigcat:compound ${molURI} ;"
+      cmpdData += "${molURI} rdfs:label \"${compound.compound}\" ;\n" +
+                  "  smiles: \"${compound.chem_struct}\" ;\n" +
+                  "  inchikey: \"${molInChI.key}\" .\n\n"
+    }
+
+    // genes
+    genesData = "";
+    if (data.cluster.genes) {
+      data.cluster.genes.annotations.each { gene ->
+        // println "#  gene: ${gene.id}"
+      }
+    }
+
+    // publicatoins
+    pubsData = "";
+    if (data.cluster.publications) {
+      data.cluster.publications.each { pub ->
+        println "  cito:cites ${pub} ;"
+      }
+    }
+
+    println "  bigcat:taxon ncbitaxon:${data.cluster.ncbi_tax_id} .\n"
+    println "ncbitaxon:${data.cluster.ncbi_tax_id} rdfs:label \"${data.cluster.organism_name}\" .\n"
+    println cmpdData; println "";
+    println genesData;
+
+    System.exit(0)
+  }
+}
